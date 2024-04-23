a396b6a60fe26af73502da0549d8c823351c0eaa
Arnihan <arnihan.manivannan123@gmail.com>
2024-04-23 10:19:15 +0200

    changes

--- game.py
+++ game.py
@@ -36,7 +36,8 @@ class Game:
         while True:
             self.display.fill((14, 219, 248))
 
-            self.scroll[0] += 1
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             
             self.tilemap.render(self.display, offset=self.scroll)
             
